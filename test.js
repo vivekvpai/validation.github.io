0a803d9ad4d23a8017ee037d03833b656bf93c46
Vivek V Pai <paivivek002@gmail.com>
2021-07-25 10:50:54 +0530

    fina;

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-//alert("connected")
-
 const userName=document.getElementById("name");
 const gYear=document.getElementById("gyear");
 const usn=document.getElementById("usn");
 const numberCode=document.getElementById("number_code");
 const phonNo=document.getElementById("contact_number");
-const form = document.getElementById("form");
+const branch=document.getElementById("branch");
+const form = document.getElementById("form1");
 
 
-form.addEventListener('mouseout', (e)=>
+form.addEventListener('click', (e)=>
 {
  e.preventDefault();
  checkName();
- checkYear();
  checkUsn();
+ checkYear();
  checkNumber();
 });
 
 function checkAll()
 {
     if (checkName() && checkYear() && checkUsn() && checkNumber()  === true )
+    //if( checkName() && checkUsn() === true )
     {
         return true;
     }
@@ -27,6 +27,7 @@ function checkAll()
     {
         return false;
     }
+    
 }
 
 function checkName()
@@ -41,22 +42,6 @@ function checkName()
     else 
     {
         setErrorFor(userName, 'Alphabets only please');
-        return false;
-    }
-}
-
-function checkYear()
-{
-    const gYearValue = gYear.value.trim();
-    let validRegex = /^2[0-9 ]+$/;
-    if (gYearValue.match(validRegex) && gYearValue.length === 4) 
-    {
-        setSucessFor(gYear, "Perfect");
-        return true;
-    }
-    else 
-    {
-        setErrorFor(gYear, "Numbers only please Starting with 2XXX");
         return false;
     }
 }
@@ -87,6 +72,24 @@ function checkUsn()
     }
 }
 
+function checkYear()
+{
+    const gYearValue = gYear.value.trim();
+    let validRegex = /^2[0-9 ]+$/;
+    if (gYearValue.match(validRegex) && gYearValue.length === 4) 
+    {
+        setSucessFor(gYear, "Perfect");
+        return true;
+    }
+    else 
+    {
+        setErrorFor(gYear, "Numbers only please Starting with 2XXX");
+        return false;
+    }
+}
+
+
+
 function checkNumber()
 {
     const phonNoValue = phonNo.value.trim();
@@ -113,7 +116,6 @@ function checkNumber()
 }
 
 
-
 function setErrorFor(input, message)
 {
     const formGroup = input.parentElement;
@@ -137,3 +139,39 @@ function setEmptyFor(input, message)
     small.innerText = message;
     formGroup.className = 'form-group empty';
 }
+
+var list1 = [];
+		var list2 = [];
+		var list3 = [];
+		var list4 = [];
+        var list5 = [];
+
+		var n = 1;
+		var x = 0;
+
+		function AddRow(){
+
+			var AddRown = document.getElementById('show');
+			var NewRow = AddRown.insertRow(n);
+
+			list1[x] = userName.value.toUpperCase() ;
+			list2[x] = usn.value.toUpperCase() ;
+			list3[x] = branch.value.toUpperCase() ;
+			list4[x] = gYear.value.toUpperCase() ;
+            list5[x] = phonNo.value.toUpperCase() ;
+
+			var cel1 = NewRow.insertCell(0);
+			var cel2 = NewRow.insertCell(1);
+			var cel3 = NewRow.insertCell(2);
+			var cel4 = NewRow.insertCell(3);
+            var cel5 = NewRow.insertCell(4);
+
+			cel1.innerHTML = list1[x];
+			cel2.innerHTML = list2[x];
+			cel3.innerHTML = list3[x];
+			cel4.innerHTML = list4[x];
+            cel5.innerHTML = list5[x];
+
+			n++;
+			x++;
+		}
